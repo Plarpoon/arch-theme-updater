b24b7399a0f4d8c9788ff02df1e8fd99f0bbb589
Alex <40299833+Plarpoon@users.noreply.github.com>
2021-07-04 01:32:41 +0200

    Add files via upload

---
+++ .theme-updater.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Pull git updates
+git -C /home/plarpoon/Repositories/Orchis-kde pull
+git -C /home/plarpoon/Repositories/Orchis-theme pull
+git -C /home/plarpoon/Repositories/Fluent-icon-theme pull
+git -C /home/plarpoon/Repositories/Vimix-cursors pull
+git -C /home/plarpoon/Repositories/virtual-desktop-bar pull
+
+# Update theme pack
+/home/plarpoon/Repositories/Orchis-kde/install.sh
+/home/plarpoon/Repositories/Orchis-theme/install.sh
+/home/plarpoon/Repositories/Fluent-icon-theme/install.sh -a
+/home/plarpoon/Repositories/Vimix-cursors/install.sh
+
+# Virtual desktop requires to CD into folder
+cd /home/plarpoon/Repositories/virtual-desktop-bar
+./scripts/install-applet.sh
